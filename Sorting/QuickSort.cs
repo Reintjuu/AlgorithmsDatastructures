67e367280471b6b24c8cd546cc1b52279decdbd8
Reinier de Vries <reiniervries@outlook.com>
2017-09-29 13:04:31 +0200

    Cleanup

--- Sorting/QuickSort.cs
+++ Sorting/QuickSort.cs
@@ -25,12 +25,11 @@ namespace ADCode.Sorting
 
 		private int Partition(int[] array, int low, int high)
 		{
-			int pivot = array[high];
 			int i = low - 1;
 
 			for (int j = low; j < high; j++)
 			{
-				if (array[j] <= pivot)
+				if (array[j] <= array[high])
 				{
 					i++;
 					Swap(array, i, j);
@@ -49,20 +48,19 @@ namespace ADCode.Sorting
 			{
 				Swap(array, low, high);
 			}
-			
+
 			if (array[mid] < array[low])
 			{
 				Swap(array, mid, low);
 			}
-			
+
 			if (array[high] < array[mid])
 			{
 				Swap(array, high, mid);
 			}
-			
+
 			return Partition(array, low, high);
 		}
-
 
 		private void Swap(int[] array, int a, int b)
 		{
