66e312152050df1217cda9f832b828d4acdda85a
Reinier de Vries <reiniervries@outlook.com>
2017-10-12 11:34:10 +0200

    Added Sieve of Eratosthenes

---
+++ PrimeNumbers/FindPrimes.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ADCode.PrimeNumbers
+{
+	public class FindPrimes
+	{
+		public bool[] SieveOfEratosthenes(uint n)
+		{
+			if (!(n > 1))
+			{
+				throw new ArgumentException("n should be bigger than 1");
+			}
+
+			bool[] isPrime = new bool[n + 1];
+			for (int i = 2; i <= n; i++)
+			{
+				isPrime[i] = true;
+			}
+			
+			for (int i = 2; i <= n; i++)
+			{
+				if (isPrime[i])
+				{
+					for (int j = i * 2; j <= n; j += i)
+					{
+						isPrime[j] = false;
+					}
+				}
+			}
+
+			return isPrime;
+		}
+	}
+}
